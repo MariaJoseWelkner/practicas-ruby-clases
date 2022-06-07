a6b52e790a1f7cd0552f9c70b268d7ea4cd15898
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-07 17:49:16 -0400

    agregando otra opcion

--- mayor_de_4_otraopcion.rb
+++ mayor_de_4_otraopcion.rb
@@ -1,26 +1,18 @@
 numero1 = ARGV[0].to_i
 numero2 = ARGV[1].to_i
 numero3 = ARGV[2].to_i
-numero4 = ARGV[2].to_i
+numero4 = ARGV[3].to_i
 
+maxNum = numero1
 
-if numero1 >= numero2
-    if numero 1 >= numero 3
-        puts numero1
-    elsif numero4 >= numero3
-        puts numero4
-    end
-elsif numero2 >= numero1
-    if numero2 >= numero3
-        puts numero2
-    else
-        puts numero3
-    end
-elsif numero3>= numero4
-    if numero3 >= numero4
-
-else
-    puts numero3
+if numero2 > maxNum
+    maxNum = numero2
+end
+if numero3 > maxNum
+    maxNum = numero3
+end
+if numero4 > maxNum
+    maxNum = numero4
 end
 
-# ejercicio maria jose villasante
+puts maxNum
