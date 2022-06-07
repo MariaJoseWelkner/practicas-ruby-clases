d1ee56fb058fed829e2cd0f7419df235b3a0f0b9
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-07 12:00:13 -0400

    ejemplo while

---
+++ mayor_de_3.rb
@@ -0,0 +1,21 @@
+numero1 = ARGV[0].to_i
+numero2 = ARGV[1].to_i
+numero3 = ARGV[2].to_i
+
+if numero1 >= numero2
+    if numero 1 >= numero 3
+        puts numero1
+    elsif numero3 >= numero1
+        puts numero3
+    end
+elsif numero2 >= numero1
+    if numero2 >= numero3
+        puts numero2
+    else
+        puts numero3
+    end
+else
+    puts numero3
+end
+
+# ejercicio maria jose villasante
