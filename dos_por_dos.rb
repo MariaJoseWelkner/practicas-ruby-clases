7f3dacf58ded230f01592a638a07dbcc17fb10ef
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-08 15:27:02 -0400

    agregando dos_por_dos.rb

--- dos_por_dos.rb
+++ dos_por_dos.rb
@@ -2,13 +2,13 @@ print "Ingresa el numero de patrones a imprimir: "
 n= gets.chomp.to_i
 
 n.times do |i|
-if i%4 == 0 || i%4 ==1
-    print "*"
-    print "cuando i vale #{i} el modulo de 4 es #{i%4}"
-else
-    print "cuando $$$ vale #{i} el modulo de 4 es #{i%4}"
+    if i%4 == 0 || i%4 ==1
+        print "*"
+        print "cuando i vale #{i} el modulo de 4 es #{i%4}"
+    else
+        print "cuando $$$ vale #{i} el modulo de 4 es #{i%4}"
 
-    print "."
-end
+        print "."
+    end
 end
 
