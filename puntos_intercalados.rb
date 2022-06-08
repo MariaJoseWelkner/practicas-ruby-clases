5a3eb8190a5a5b99286eab3ad7505377e3c2aec0
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-08 10:42:09 -0400

    agregando puntos_intercalados.rb

--- puntos_intercalados.rb
+++ puntos_intercalados.rb
@@ -5,6 +5,7 @@ n= gets.chomp.to_i
 
 (1..n).each do |var_block|
 
+    # metodo even:
     # even cuando es par y odd cuando es impar
     if var_block.even?
         print var_block
@@ -15,3 +16,11 @@ n= gets.chomp.to_i
 end
 
 # do, if son aperturas de etiqueta de apertura que necesitan ir cerradas con un end
+
+
+# con el metodo del modulo, para que me de el resto de la division: (si el resto es 0 la division es par, si tiene resto es impar)
+# if var_block%2 ==0
+#     print var_block
+# else
+#     print"."
+# end
