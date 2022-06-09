def77dc20b27aaad5d1c7340895eb2e3dfa498be
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-09 13:01:27 -0400

    agregando simple_iterator_array.rb

---
+++ simple_iterator_array.rb
@@ -0,0 +1,15 @@
+a = [4,3,2,7]
+sumaVal = 0
+suma2 = 0
+a.each do |i|
+    sumaVal = sumaVal +i 
+end 
+puts sumaVal
+b= [*1..100]
+# con dos puntos incluye del 1 al 100 con 3 puntos empieza en uno pero no incluye el ultimo ,el 100
+b.each{|j| suma2 = suma2 + j }
+puts suma2
+
+# b.each do |j|
+#     suma2 = suma2 + j 
+# end 
