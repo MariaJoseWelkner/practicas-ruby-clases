bc85dba5b9e99f64390c9169edc2e65cd600dba8
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-09 17:05:05 -0400

    agregando simple_filter_array.eb

---
+++ adicto_a_redes.rb
@@ -0,0 +1,7 @@
+minutos = [90,120,500,200,50,30,45,700]
+resultado = []
+
+def scan_addicts(minutos)
+
+end
+
---
+++ simple_filter_array.rb
@@ -0,0 +1,12 @@
+numeros = [*1..1000]
+numeros_pares = []
+#  puts numeros.select {|num| num.even? ?  "par" :  "impar"}numero.each do |j| 
+numeros.each do |j|
+    if  j.even? 
+           end
+           end
+           print numeros_pares
+           
+           numeros = [*1..1000]
+numeros_pares = numeros.select { |num| num.even?}
+print numeros_pares
