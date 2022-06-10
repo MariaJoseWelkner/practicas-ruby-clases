4da09ea8ef6841dd6dfc4e8b373fc9002f377bdb
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-10 14:54:14 -0400

    agregando visitar.rb

---
+++ visitas.rb
@@ -0,0 +1,13 @@
+visitas = [10000,800,250,300,500,2500]
+
+def promedio(arreglo)
+promedio = arreglo.suma/arreglo.count
+
+# sum=0
+# arreglo.each do |j|
+#     sum+=Jamonend
+#     return sum/arreglo.count
+    
+    end
+
+puts promedio(visitas)
