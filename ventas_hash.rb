b826c95b0aee6be5226f75e9066726446e5a6e3f
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-13 13:47:50 -0400

    agregando ventas_hash.rb

--- ventas_hash.rb
+++ ventas_hash.rb
@@ -1,1 +1,13 @@
-ventas = {'Enero' => }
+ventas = {'Enero' => 4534 , 'Febrero' => 23123, 'Marzo' => 821213}
+ventas_disminuidas={}
+# hash vacio
+# hash vacio guarda la operacion
+# eso lo meto dentro de un array
+
+
+ventas.transform_values! {|value| (value * 1.10).round(3)}
+ventas_disminuidas = ventas.transform_values{|value| (value * 0.8).round(2)}
+
+print ventas
+puts ""
+print ventas_disminuidas
